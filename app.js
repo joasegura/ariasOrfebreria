e6c63eacc42fddb49cb59c1d10c5d6ca045f6c40
Joaquin <joaquin.segura98@gmail.com>
2025-04-07 22:46:07 -0300

    Descripción breve de los cambios

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('a[href="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
